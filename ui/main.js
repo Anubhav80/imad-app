377ce78918ec9441ec9c7ee8c94cecf7937b7820
Anubhav80 <anu.ritz80@gmail.com>
2018-02-20 02:05:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,15 @@
-console.log("function start");
+
 var button=document.getElementById('counter');
  button.onclick = function(){
      console.log("function start");
     var request = new XMLHttpRequest();
-    console.log("function start1");
     
     request.onreadystatechange = function(){
-        console.log("function start2");
-      if(request.readystate === XMLHttpRequest.DONE){
-          console.log("function start3");
+     
+      if(request.readyState === XMLHttpRequest.DONE){
+
           if(request.status === 200){
-              console.log("function start4");
+            
               var counter = request.responseText;
           var span = document.getElementById('count');
     span.innerHTML = counter.toString();
